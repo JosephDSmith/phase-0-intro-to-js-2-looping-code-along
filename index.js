90bd032bf1361aa0b4b062175260d33f2a56a201
Joseph Smith <joesmith40@gmail.com>
2023-01-21 21:52:18 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const messages = []
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i]
+      const message = `Thank you, ${name}, for the wonderful ${event} gift!`
+      messages.push(message)
+    }
+    return messages
+}
+let num = 4
+function countDown(num) {
+    while (num >= 0) {
+    console.log(num)
+    num--
+    }
+}
+
+
